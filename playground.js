ea65582cc263e8834b2058e867abbc3a0361d67b
JustBBox <bloodlybox@gmail.com>
2021-10-17 03:32:55 +0300

    async/await function implemented

--- playground.js
+++ playground.js
@@ -109,8 +109,8 @@ reduce(asyncArray, reducerSum, 0, (res) => {
     Импорты. Весь вспомогательный код следует положить в замыкание (см. оформление)
     Комментарии в коде не проходят автотесты */
 
-//Calback Hell Variant
-function reduce(asyncArray, fn, initialValue, cb) {
+//Calback Hell Variant Actually Working Fine
+function reduceCallbackHellEdition(asyncArray, fn, initialValue, cb) {
     asyncArray.length((length_) => {
 
         let callbackStep = (acc, cur, index, arr, cb) => {
@@ -146,4 +146,43 @@ function reduce(asyncArray, fn, initialValue, cb) {
             }
         });
     })
+}
+
+
+function getter(fn, a, b, c, d) {
+    switch (arguments.length) {
+        case 1:
+            return new Promise(resolve => (fn(resolve)));
+        case 2:
+            return new Promise(resolve => (fn(a, resolve)));
+        case 3:
+            return new Promise(resolve => (fn(a, b, resolve)));
+        default:
+            return new Promise(resolve => (fn(a, b, c, d, resolve)));
+    }
+}
+
+// Async/Await
+async function reduce(asyncArray, fn, initialValue, cb) {
+    let length_ = await getter(asyncArray.length);
+    let compare = await getter(Homework.equal, 0, length_);
+    if(compare) {
+        compare = await getter(Homework.equal, typeof length_, 'number');
+        if(compare) {
+            cb(initialValue);
+        } else {
+            throw new Error('Reduce of empty array with no initial value');
+        }
+    } else {
+        let acc = initialValue || 0;
+        let index = 0;
+        let conditionStop = await getter(Homework.less, index, length_);
+        while (conditionStop) {
+            let cur = await getter(asyncArray.get, index);
+            acc = await getter(fn, acc, cur, index, asyncArray);
+            index++;
+            conditionStop = await getter(Homework.less, index, length_);
+        }
+        cb(acc);
+    }
 }
--- solution/index.js
+++ solution/index.js
@@ -1,43 +1,37 @@
-// solution/index.js
 module.exports = function (Homework) {
-
-    // вспомогательные фукнции и т.д.
-
-    return (asyncArray, fn, initialValue, cb) => {
-        asyncArray.length((length_) => {
-
-            let callbackStep = (acc, cur, index, arr, cb) => {
-                Homework.less(index, length_, (compareResult) => {
-                    if (compareResult) {
-                        arr.get(index, (cur) => {
-                            fn(acc, cur, index, arr, (elem) => {
-                                Homework.add(index, 1, (incrementedIndex) => {
-                                    callbackStep(elem, cur, incrementedIndex, arr, cb);
-                                })
-                            });
-                        })
-                    } else {
-                        cb(acc);
-                    }
-                })
-            };
-
-            Homework.equal(0, length_, (compareResult) => {
-                if (compareResult) {
-                    Homework.equal(typeof length_, 'number', (compareResult) => {
-                        if(compareResult) {
-                            cb(initialValue);
-                        } else {
-                            throw new Error('Reduce of empty array with no initial value');
-                        }
-                    })
-                } else {
-                    let acc = initialValue || 0;
-                    asyncArray.get(0, (cur) => {
-                        callbackStep(acc, cur, 0, asyncArray, cb);
-                    })
-                }
-            });
-        })
+    function getter(fn, a, b, c, d) {
+        switch (arguments.length) {
+            case 1:
+                return new Promise(resolve => (fn(resolve)));
+            case 2:
+                return new Promise(resolve => (fn(a, resolve)));
+            case 3:
+                return new Promise(resolve => (fn(a, b, resolve)));
+            default:
+                return new Promise(resolve => (fn(a, b, c, d, resolve)));
+        }
+    }
+    return async (asyncArray, fn, initialValue, cb) => {
+        let length_ = await getter(asyncArray.length);
+        let compare = await getter(Homework.equal, 0, length_);
+        if(compare) {
+            compare = await getter(Homework.equal, typeof length_, 'number');
+            if(compare) {
+                cb(initialValue);
+            } else {
+                throw new Error('Reduce of empty array with no initial value');
+            }
+        } else {
+            let acc = initialValue || 0;
+            let index = 0;
+            let conditionStop = await getter(Homework.less, index, length_);
+            while (conditionStop) {
+                let cur = await getter(asyncArray.get, index);
+                acc = await getter(fn, acc, cur, index, asyncArray);
+                index++;
+                conditionStop = await getter(Homework.less, index, length_);
+            }
+            cb(acc);
+        }
     }
 }
